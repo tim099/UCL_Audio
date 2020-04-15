f797fa1d1cf6f958dc4ac978329402c3ce872409
tim099 <tim11251994@gmail.com>
2020-04-15 16:56:37 +0800

    Add UCL_StreamingAudioClip.cs Add UCL_StreamingAudioSource.cs

---
+++ UCL_AudioScript/UCL_StreamingAudioClip.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCL.AudioLib {
+    public class UCL_StreamingAudioClip : MonoBehaviour {
+        public struct AudioData {
+            public AudioData(float[] _data) {
+                m_Data = _data;
+            }
+            public float[] m_Data;
+        }
+        public int m_LengthSamples = 1024;
+        public int m_Channels = 1;
+        public int m_Frequency = 8000;
+        //public bool m_3D = false;
+        public bool m_Stream = false;
+
+        public AudioClip m_Clip { get; protected set; }
+
+        protected Queue<AudioData> m_AudioDatas;
+
+        bool f_Inited = false;
+        virtual public void Init() {
+            if(f_Inited) return;
+            f_Inited = true;
+            m_AudioDatas = new Queue<AudioData>();
+            m_Clip = AudioClip.Create(name, m_LengthSamples, m_Channels, m_Frequency, m_Stream);
+        }
+        private void OnDestroy() {
+            if(m_Clip) {
+                Destroy(m_Clip);
+                m_Clip = null;
+            }
+        }
+        public int GetDataCount() {
+            return m_AudioDatas.Count;
+        }
+        public UCL_StreamingAudioClip AddData(float[] data) {
+            m_AudioDatas.Enqueue(new AudioData(data));
+            return this;
+        }
+        public bool LoadData() {
+            if(m_AudioDatas.Count == 0) return false;
+            var data = m_AudioDatas.Dequeue();
+            m_Clip.SetData(data.m_Data, 0);
+            return true;
+        }
+    }
+}
+
---
+++ UCL_AudioScript/UCL_StreamingAudioSource.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCL.AudioLib {
+    public class UCL_StreamingAudioSource : MonoBehaviour {
+        AudioSource m_Source;
+        UCL_StreamingAudioClip m_StreamingAudioClip;
+        public bool m_Playing = false;
+
+        static public UCL_StreamingAudioSource Create(Transform parent) {
+            GameObject Obj = new GameObject("StreamingAudioSource");
+            Obj.transform.SetParent(parent);
+            var src = Obj.AddComponent<UCL_StreamingAudioSource>();
+            return src;
+        }
+
+        bool f_Inited = false;
+        virtual public void Init() {
+            if(f_Inited) return;
+            f_Inited = true;
+
+            m_StreamingAudioClip = gameObject.AddComponent<UCL_StreamingAudioClip>();
+            m_StreamingAudioClip.Init();
+
+            m_Source = gameObject.AddComponent<AudioSource>();
+            m_Source.clip = m_StreamingAudioClip.m_Clip;
+            m_Source.loop = false;
+        }
+        public void Play() {
+            m_Playing = true;
+            if(f_Inited) return;
+
+        }
+        public void Pause() {
+            m_Playing = false;
+            if(f_Inited) return;
+            m_Source.Pause();
+        }
+        public UCL_StreamingAudioSource AddData(float[] data) {
+            if(m_StreamingAudioClip == null) return this;
+            m_StreamingAudioClip.AddData(data);
+            return this;
+        }
+        public int GetDataCount() {
+            if(m_StreamingAudioClip == null) return 0;
+            return m_StreamingAudioClip.GetDataCount();
+        }
+        // Update is called once per frame
+        void Update() {
+            if(m_Playing) {
+                if(!m_Source.isPlaying) {
+                    if(m_StreamingAudioClip.LoadData()) {
+                        m_Source.Play();
+                    }
+                }
+            }
+        }
+    }
+}
